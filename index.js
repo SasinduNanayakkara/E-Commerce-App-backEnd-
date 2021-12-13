9913e376e0f51c94e4dddfb1598dee2741d445c9
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-14 02:02:49 +0530

    initialize routers

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Express = require("express");
 const mongoose = require('mongoose');
 const app = Express();
 const dotenv = require('dotenv');
+const userRoute = require("./routes/user");
+const express = require("express");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -9,6 +12,16 @@ mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection su
 ).catch((err) =>{
     console.log(err);
 });
+
+app.use(express.json());
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+
+app.get("/api/test", ()=>{
+    console.log("Test successful");
+});
+
 
 app.listen(process.env.PORT || 3500, ()=>{
     console.log("BackEnd Server is Running");
