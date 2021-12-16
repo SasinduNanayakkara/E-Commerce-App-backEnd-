d9af0275a051b82c44b7446e33cd33442702cd4f
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-16 22:04:12 +0530

    index file

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
 const cartRoute = require("./routes/cart");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection successful")
 ).catch((err) =>{
     console.log(err);
 });
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/checkout", stripeRoute);
 
 
 app.get("/api/test", ()=>{
